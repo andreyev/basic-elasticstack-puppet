b0637b2cac3a091df19b138d240b8cf8cc2dbb55
Andreyev Dias de Melo <andreyev@users.noreply.github.com>
2017-06-29 21:49:38 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/andreyev/one-vagrantfile-to-rule-them-all/first-commit/puppet-installer | bash
 LIBRARIAN_PUPPET_PATH=/opt/puppetlabs/modules LIBRARIAN_PUPPET_TMP=/opt/puppetlabs/tmp /opt/puppetlabs/puppet/bin/librarian-puppet install --verbose
-puppet apply --modulepath /opt/puppetlabs/modules  init.pp
+/opt/puppetlabs/bin/puppet apply --modulepath /opt/puppetlabs/modules  init.pp
