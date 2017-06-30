9cdd26ca6b7d9b4599d43310f490efb8787d6cdf
Andreyev Dias de Melo <andreyev.dias@adtsys.com.br>
2017-06-30 06:34:32 -0300

    remove indireção

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/andreyev/one-vagrantfile-to-rule-them-all/first-commit/puppet-installer | bash
+which puppet || {
+  OS_MAJ_VERSION=$(rpm -qa \*-release | grep -Ei "centos" | cut -d"-" -f3)
+  [[ -z "$OS_MAJ_VERSION" ]] && { echo "Distro not supported"; exit 1;}
+  yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-"$OS_MAJ_VERSION".noarch.rpm
+  yum install -y puppet
+}
+
+which librarian-puppet || {
+  /opt/puppetlabs/puppet/bin/gem install  librarian-puppet
+}
 LIBRARIAN_PUPPET_PATH=/opt/puppetlabs/modules LIBRARIAN_PUPPET_TMP=/opt/puppetlabs/tmp /opt/puppetlabs/puppet/bin/librarian-puppet install --verbose
 puppet apply --modulepath /opt/puppetlabs/modules  init.pp
