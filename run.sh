f54215439568605de113fdcb4a5e7886b761b895
Andreyev Dias de Melo <andreyev@users.noreply.github.com>
2017-07-01 11:00:40 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-which puppet || {
-  OS_MAJ_VERSION=$(rpm -qa \*-release | grep -Ei "centos" | cut -d"-" -f3)
-  [[ -z "$OS_MAJ_VERSION" ]] && { echo "Distro not supported"; exit 1;}
-  yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-"$OS_MAJ_VERSION".noarch.rpm
-  yum install -y puppet
-}
+OS_MAJ_VERSION=$(rpm -qa \*-release | grep -Ei "centos" | cut -d"-" -f3)
+[[ -z "$OS_MAJ_VERSION" ]] && { echo "Distro not supported"; exit 1;}
+yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-"$OS_MAJ_VERSION".noarch.rpm
+yum install -y puppet git
 
-which librarian-puppet || {
-  /opt/puppetlabs/puppet/bin/gem install  librarian-puppet
-}
+
+cd basic-elasticstack-puppet
+/opt/puppetlabs/puppet/bin/gem install  librarian-puppet
+
 LIBRARIAN_PUPPET_PATH=/opt/puppetlabs/modules LIBRARIAN_PUPPET_TMP=/opt/puppetlabs/tmp /opt/puppetlabs/puppet/bin/librarian-puppet install --verbose
 /opt/puppetlabs/bin/puppet apply --modulepath /opt/puppetlabs/modules  init.pp
